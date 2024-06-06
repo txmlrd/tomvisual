fb6b30a34f1e9c20e48337c61698725e1dc6189f
txmlrd <gungadhisanjaya@gmail.com>
2024-06-06 14:44:07 +0700

    feat: overall routing web

---
+++ src/pages/sandbox/landingpage/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href='link'>Home</Link>
+        </li>
+        <li>
+          <Link href='link'>Home</Link>
+        </li>
+        <li>
+          <Link href='link'>Home</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
